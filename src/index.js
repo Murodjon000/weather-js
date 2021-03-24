c494349d71f71cf3f70d977341249d4ef58467ae
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-24 19:23:59 +0500

    update index js file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import "bootstrap";
-import "jquery";
-import fetchWeather from "./api/api.js";
-import weatherUpdate from "./view/weather.js";
-const searchForm = document.getElementById("form");
-const citySearch = document.getElementById("citySearch");
-var units = "metric";
+import 'bootstrap';
+import 'jquery';
+import fetchWeather from './api/api';
+import weatherUpdate from './view/weather';
 
-searchForm.addEventListener("submit", (e) => {
+const searchForm = document.getElementById('form');
+const citySearch = document.getElementById('citySearch');
+const units = 'metric';
+
+searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const cityValue = citySearch.value;
   searchForm.reset();
-  console.log(cityValue);
   fetchWeather(cityValue)
     .then((data) => {
       weatherUpdate(data, units);
     })
     .catch((err) => {
-      document.querySelector(".error-text").textContent = err.message;
+      document.querySelector('.error-text').textContent = err.message;
     });
 });
 
-window.addEventListener("DOMContentLoaded", (e) => {
-  e.preventDefault();
-  fetchWeather("London")
+window.onload = async () => {
+  fetchWeather('London')
     .then((data) => {
       weatherUpdate(data, units);
     })
     .catch((err) => {
-      return err;
+      document.querySelector('.error-text').textContent = err.message;
     });
-});
+};
