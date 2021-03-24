fa290e3cf6f857d1d2b83bfc1be7336f5d3dc9cf
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-24 19:25:00 +0500

    update npm package

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const dotenv = require("dotenv").config({
-  path: path.join(__dirname, ".env"),
+const path = require('path');
+const dotenv = require('dotenv').config({
+  path: path.join(__dirname, '.env'),
 });
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
+      $: 'jquery',
+      jQuery: 'jquery',
     }),
     new webpack.DefinePlugin({
-      "process.env": dotenv.parsed,
+      'process.env': dotenv.parsed,
     }),
   ],
 };
