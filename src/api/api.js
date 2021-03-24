647d59a4ec48fab8874cef3f8869c0f10d26acc1
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-24 19:23:37 +0500

    update api js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,18 @@
-const units = "metric";
+const units = 'metric';
+const weather = 'weather';
 const apiKey = process.env.APIKEY;
+
 const fetchWeather = async (city) => {
-  const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const baseUrl = `https://api.openweathermap.org/data/2.5/${weather}`;
   const query = `?q=${city}&units=${units}&appid=${apiKey}`;
   const response = await fetch(baseUrl + query);
   const data = await response.json();
   if (data.cod === 200) {
-    document.querySelector(".error-text").style.display = "none";
+    document.querySelector('.error-text').style.display = 'none';
     return data;
-  } else {
-    document.querySelector(".error-text").style.display = "block";
-    throw new Error("City not found. Please try again.");
   }
+  document.querySelector('.error-text').style.display = 'block';
+  throw new Error('City not found. Please try again.');
 };
 
 export default fetchWeather;
