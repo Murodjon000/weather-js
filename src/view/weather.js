8030f34dd271be83b1e1c4f2bb5b707e53d0d297
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-25 18:32:39 +0500

    add forecast update methods

--- src/view/weather.js
+++ src/view/weather.js
@@ -13,7 +13,7 @@ const pressureMes = document.querySelector('.pressure-mes');
 const humadityTitle = document.querySelector('.humadity-title');
 const humadityMes = document.querySelector('.humadity-mes');
 const changeBox = document.querySelector('.change-box');
-
+const button = document.createElement('button');
 const currentCityTime = (timezone) => {
   const date = new Date();
   const utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
@@ -33,13 +33,29 @@ const currentCityTime = (timezone) => {
   ).toLocaleDateString('en-US', format);
   return foreignTime;
 };
-const button = document.createElement('button');
+
+const tempChange = (temp, tempC, tempF, units) => button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const elm = e.target;
+  if (units === 'imperial') {
+    temp = tempC;
+    cityDegree.innerHTML = `${temp}&degC`;
+    units = 'metric';
+    elm.innerHTML = 'Fahrenheit';
+  } else {
+    temp = tempF;
+    cityDegree.innerHTML = `${temp}&degF`;
+    units = 'imperial';
+    elm.innerHTML = 'Celcius';
+  }
+});
+
 const weatherUpdate = async (data, units) => {
   const { name, timezone } = data;
   const { description, main, icon } = data.weather[0];
   const { speed } = data.wind;
   const { humidity, pressure } = data.main;
-  let { temp } = data.main;
+  const { temp } = data.main;
   cityName.innerHTML = name;
   cityWeather.innerHTML = description;
   windSpeed.innerHTML = speed;
@@ -54,28 +70,56 @@ const weatherUpdate = async (data, units) => {
   humadityMes.innerHTML = '%';
   pressureTitle.innerHTML = 'Pressure';
   pressureMes.innerHTML = 'hPa';
-  document.querySelector('body').style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${main}')`;
+  document.querySelector(
+    'body',
+  ).style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${main}')`;
   changeBox.appendChild(button);
   const classes = ['btn', 'btn-outline-warning', 'px-3', 'mr-2', 'mt-2'];
   button.classList.add(...classes);
   button.innerHTML = 'Fahrenheit';
   const tempF = (temp * 1.8 + 32).toFixed(2);
   const tempC = temp;
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    const elm = e.target;
-    if (units === 'imperial') {
-      temp = tempC;
-      cityDegree.innerHTML = `${temp}&degC`;
-      units = 'metric';
-      elm.innerHTML = 'Fahrenheit';
-    } else {
-      temp = tempF;
-      cityDegree.innerHTML = `${temp}&degF`;
-      units = 'imperial';
-      elm.innerHTML = 'Celcius';
-    }
-  });
+  tempChange(temp, tempC, tempF, units);
 };
 
-export default weatherUpdate;
+const forecastUpdateStyle = (forecast) => {
+  const forecastBox = document.querySelector('.forecast-box');
+  forecastBox.innerHTML = '';
+  for (let i = 0; i < forecast.length; i += 1) {
+    const oneDay = document.createElement('div');
+    const classes = ['px-3', 'text-center'];
+    oneDay.classList.add(...classes);
+    const element = forecast[i];
+    const options = {
+      weekday: 'long',
+    };
+    const weekDay = element.des_weekday.toLocaleString('en-US', options);
+    oneDay.innerHTML = `
+    <p class='text-center mb-0'>${weekDay}</p>
+    <img src="https://openweathermap.org/img/wn/${element.weather_icon}.png" class="text-center" alt="weather-icon">
+    <p class='text-center mb-0 city-deg'>${element.temperature}&deg</p>
+    `;
+
+    forecastBox.appendChild(oneDay);
+  }
+};
+
+const forecastUpdate = (data) => {
+  const forecast = [];
+  const dataArray = data.data.list;
+
+  for (let i = 3; i < dataArray.length; i += 8) {
+    const oneDay = {
+      weather: dataArray[i].weather[0].main,
+      description: dataArray[i].weather[0].description,
+      weather_icon: dataArray[i].weather[0].icon,
+      temperature: Math.round(dataArray[i].main.temp),
+      des_weekday: new Date(dataArray[i].dt_txt),
+    };
+    forecast.push(oneDay);
+  }
+
+  return forecastUpdateStyle(forecast);
+};
+
+export { weatherUpdate, forecastUpdate };
