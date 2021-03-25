28e003591fe3de1ecff9a20491b833746aca656d
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-25 18:32:02 +0500

    add 5 days forecast api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 const units = 'metric';
 const weather = 'weather';
+const forecast = 'forecast';
 const apiKey = 'f58a5dabce15f90651f48675d3fccb22';
 
 const fetchWeather = async (city) => {
@@ -15,4 +16,11 @@ const fetchWeather = async (city) => {
   throw new Error('City not found. Please try again.');
 };
 
-export default fetchWeather;
+const fetchWeatherForecast = async (city) => {
+  const baseUrl = `https://api.openweathermap.org/data/2.5/${forecast}`;
+  const query = `?q=${city}&units=${units}&appid=${apiKey}`;
+  const response = await fetch(baseUrl + query);
+  const data = await response.json();
+  return { data };
+};
+export { fetchWeather, fetchWeatherForecast };
